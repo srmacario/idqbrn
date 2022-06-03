36c9b56b70426663553661b6a5133d229e83638f
phmontav <phmontav@gmail.com>
2022-06-03 19:00:25 -0300

    server request

--- src/components/MapDashboard.js
+++ src/components/MapDashboard.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import Map from './Map'
 import MenuNav from './MenuNav'
+import axios from 'axios'
 
-function MapDashboard() {
+class MapDashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dados: [],
+      cidades_opt: [],
+      
+    }
+  }
+  async componentDidMount() {
+    axios.get('http://localhost:8080/dados/')
+      .then(response => {
+        this.setState({ dados: response.data });
+        if (this.state.cidades_opt.length == 0) {
+          response.data.forEach(element => {
+            this.state.cidades_opt.push({ label: element['Municipio'], value: element['Municipio'] })
+          });
+        }
+
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  render() {
+    const dados = this.state.dados;
+    const cidades_opt = this.state.cidades_opt;
+    console.log("mapds");
+    console.log(dados);
+    console.log(cidades_opt);
     return (
       <>
-        <MenuNav/>
-        <Map/>
+        <MenuNav dados={dados} cidades_opt={cidades_opt} />
+        <Map />
       </>
     );
+  }
 }
 
 export default MapDashboard;
--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -1,43 +1,137 @@
 import React from 'react'
+import Select from 'react-select'
 import ImeLogo from './img/imeLogo.png'
+import SearchBox from './SearchBox'
 
-export default function MenuNav(){
-    return(
-        <div className = "menuNav">
-            <div className = "titulo">
-                <img className = "logo" src = {ImeLogo}/>
+// class MenuNav extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             dados: props.dataParentToChild
+//         }
+//     }
+//     render() {
+//         console.log('stdados' + this.state.dados);
+//         const dados = this.state.dados;
+//         console.log("menuNav");
+//         console.log(dados);
+//         return (
+//             <div className="menuNav">
+//                 <div className="titulo">
+//                     <img className="logo" src={ImeLogo} />
+//                 </div>
+
+//                 <div className="container">
+//                     <div className="wrap">
+//                         <div className="search">
+//                             {/* <SearchBox type="search" className = "searchTerm" placeholder="Buscar..." 
+//                             handleChange={(e) => console.log(e.target.value)}/> */}
+//                             <Select options={dados}
+
+//                             />
+//                             <button type="submit" className="searchButton">
+//                                 <i className="fa fa-search"></i>
+//                             </button>
+//                         </div>
+//                     </div>
+
+//                     <div className="containerList">
+//                         <ul className="ks-cboxtags">
+//                             <li><input type="checkbox" id="checkboxOne" value="Dengue" /><label for="checkboxOne">Dengue</label>
+//                             </li>
+//                             <li><input type="checkbox" id="checkboxTwo" value="Febre Amarela" checked /><label
+//                                 for="checkboxTwo">Febre Amarela</label></li>
+//                             <li><input type="checkbox" id="checkboxThree" value="Esquistossomose" checked /><label
+//                                 for="checkboxThree">Esquistossomose</label></li>
+//                             <li><input type="checkbox" id="checkboxFour" value="Malária" /><label
+//                                 for="checkboxFour">Malária</label></li>
+//                             <li><input type="checkbox" id="checkboxFive" value="Tuberculose" /><label
+//                                 for="checkboxFive">Tuberculose</label></li>
+//                             <li><input type="checkbox" id="checkboxSix" value="Leishmaniose" checked /><label
+//                                 for="checkboxSix">Leishmaniose</label></li>
+//                             <li><input type="checkbox" id="checkboxSeven" value="Doença de Chagas" /><label
+//                                 for="checkboxSeven">Doença de Chagas</label></li>
+//                             <li><input type="checkbox" id="checkboxEight" value="Leptospirose" /><label
+//                                 for="checkboxEight">Leptospirose</label></li>
+//                             <li><input type="checkbox" id="checkboxNine" value="Arboviroses Urbanas" /><label
+//                                 for="checkboxNine">Arboviroses Urbanas</label></li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+// export default MenuNav;
+
+// const filterOptions = (
+//     candidate: {label:string, value:tring}},
+//     input:string) =>{
+
+//     }
+// )
+
+
+
+export default function MenuNav(props) {
+    const dados = props.dados;
+    const cidades_opt = props.cidades_opt;
+    const filterConfig = {
+        ignoreCase: false,
+        ignoreAccents: true,
+        trim: true,
+        matchFrom: 'start',
+        stringify: option => `${option.label} ${option.value}`
+    };
+    //var cidades_opt = [];
+    console.log('menunav');
+    console.log(dados);
+    console.log(cidades_opt);
+    // dados.forEach(element => {
+    //     cidades_opt.push({label: element['Municipio'], value: element['Municipio']});
+    // });
+    console.log(cidades_opt);
+    return (
+        <div className="menuNav">
+            <div className="titulo">
+                <img className="logo" src={ImeLogo} />
             </div>
-            
-            <div className = "container">
-                <div className = "wrap">
-                    <div className = "search">
-                        <input type="text" className = "searchTerm" placeholder="Buscar..."/>
-                        <button type="submit" className = "searchButton">
-                            <i className = "fa fa-search"></i>
+
+            <div className="container">
+                <div className="wrap">
+                    <div className="search">
+                        {/* <SearchBox type="search" className = "searchTerm" placeholder="Buscar..." 
+                        handleChange={(e) => console.log(e.target.value)}/> */}
+                        <Select options={cidades_opt} placeholder="Buscar..."
+                            openMenuOnClick={true} filterConfig={filterConfig}
+
+                        />
+                        <button type="submit" className="searchButton">
+                            <i className="fa fa-search"></i>
                         </button>
                     </div>
                 </div>
 
-                <div className = "containerList">
-                    <ul className = "ks-cboxtags">
-                        <li><input type="checkbox" id="checkboxOne" value="Dengue"/><label for="checkboxOne">Dengue</label>
+                <div className="containerList">
+                    <ul className="ks-cboxtags">
+                        <li><input type="checkbox" id="checkboxOne" value="Dengue" /><label for="checkboxOne">Dengue</label>
                         </li>
-                        <li><input type="checkbox" id="checkboxTwo" value="Febre Amarela" checked/><label
-                                for="checkboxTwo">Febre Amarela</label></li>
-                        <li><input type="checkbox" id="checkboxThree" value="Esquistossomose" checked/><label
-                                for="checkboxThree">Esquistossomose</label></li>
-                        <li><input type="checkbox" id="checkboxFour" value="Malária"/><label
-                                for="checkboxFour">Malária</label></li>
-                        <li><input type="checkbox" id="checkboxFive" value="Tuberculose"/><label
-                                for="checkboxFive">Tuberculose</label></li>
-                        <li><input type="checkbox" id="checkboxSix" value="Leishmaniose" checked/><label
-                                for="checkboxSix">Leishmaniose</label></li>
-                        <li><input type="checkbox" id="checkboxSeven" value="Doença de Chagas"/><label
-                                for="checkboxSeven">Doença de Chagas</label></li>
-                        <li><input type="checkbox" id="checkboxEight" value="Leptospirose"/><label
-                                for="checkboxEight">Leptospirose</label></li>
-                        <li><input type="checkbox" id="checkboxNine" value="Arboviroses Urbanas"/><label
-                                for="checkboxNine">Arboviroses Urbanas</label></li>
+                        <li><input type="checkbox" id="checkboxTwo" value="Febre Amarela" checked /><label
+                            for="checkboxTwo">Febre Amarela</label></li>
+                        <li><input type="checkbox" id="checkboxThree" value="Esquistossomose" checked /><label
+                            for="checkboxThree">Esquistossomose</label></li>
+                        <li><input type="checkbox" id="checkboxFour" value="Malária" /><label
+                            for="checkboxFour">Malária</label></li>
+                        <li><input type="checkbox" id="checkboxFive" value="Tuberculose" /><label
+                            for="checkboxFive">Tuberculose</label></li>
+                        <li><input type="checkbox" id="checkboxSix" value="Leishmaniose" checked /><label
+                            for="checkboxSix">Leishmaniose</label></li>
+                        <li><input type="checkbox" id="checkboxSeven" value="Doença de Chagas" /><label
+                            for="checkboxSeven">Doença de Chagas</label></li>
+                        <li><input type="checkbox" id="checkboxEight" value="Leptospirose" /><label
+                            for="checkboxEight">Leptospirose</label></li>
+                        <li><input type="checkbox" id="checkboxNine" value="Arboviroses Urbanas" /><label
+                            for="checkboxNine">Arboviroses Urbanas</label></li>
                     </ul>
                 </div>
             </div>
