929fb95d8fbbfc5ee332030069fcd070eee31716
phmontav <phmontav@gmail.com>
2022-06-04 19:21:03 -0300

    filter buttons

--- src/components/MapDashboard.js
+++ src/components/MapDashboard.js
@@ -10,16 +10,19 @@ class MapDashboard extends React.Component {
     super();
     this.state = {
       dados: [],
-      cidades_opt: []
+      cidades_opt: [],
+      city_filter: [],
+      
     }
   }
   async componentDidMount() {
     axios.get('http://localhost:8080/dados/')
       .then(response => {
-        this.setState({ dados: response.data });
+        this.setState({ dados: response.data, dados_filtrados: response.data });
         if (this.state.cidades_opt.length === 0) {
-          response.data.forEach(element => {
+          response.data.forEach( element  =>  {
             this.state.cidades_opt.push({ label: element['Municipio'], value: element['Municipio'] })
+            // this.state.name_to_doc.set(element['Municipio'],element).catch(e => console.log(e));
           });
         }
 
@@ -31,12 +34,16 @@ class MapDashboard extends React.Component {
   render() {
     const dados = this.state.dados;
     const cidades_opt = this.state.cidades_opt;
+    const city_filter = this.state.city_filter;
+    const dados_filtrados = this.state.dados_filtrados;
     console.log("mapds");
-    console.log(dados);
-    console.log(cidades_opt);
+    //console.log("dados" + dados);
+    //console.log("cidade_opt" + cidades_opt);
     return (
       <>
-        <MenuNav markersArray={this.props.markersArray} setMarkers={this.props.setMarkers} navigate={this.props.navigate} dados={dados} cidades_opt={cidades_opt} />
+        <MenuNav markersArray={this.props.markersArray} setMarkers={this.props.setMarkers} 
+        navigate={this.props.navigate} dados={dados} cidades_opt={cidades_opt} 
+        city_filter={city_filter} dados_filtrados={dados_filtrados} />
         <Mapa markersArray={this.props.markersArray} />
       </>
     );
