c2fd8ddb0941ad0b205955cd01d5a34f4356c334
malmeidaw <matheus.997willians@outlook.com>
2022-06-04 22:40:11 -0300

    Alterei o css

--- src/components/UpdateInformation.js
+++ src/components/UpdateInformation.js
@@ -26,11 +26,11 @@ function UpdateInformations(){
             </div>
             <div className={stylesCorpo.corpo}>
                 <form onSubmit={UpdateInfo}>
-                    <textarea className={stylesCorpo.insertText1} name="formasdecontagio" placeholder="formas de Contágio" cols="50" row="2" onChange = {handleEntry}></textarea>
+                    <textarea className={stylesCorpo.insertText1} name="formasdecontagio" placeholder="Formas de Contágio." cols="50" row="2" onChange = {handleEntry}></textarea>
                     <p></p>
-                    <textarea className={stylesCorpo.insertText2} name="sintomas" placeholder="Sintomas" cols="50" row="10" onChange = {handleEntry}></textarea>
+                    <textarea className={stylesCorpo.insertText2} name="sintomas" placeholder="Sintomas." cols="50" row="10" onChange = {handleEntry}></textarea>
                     <p></p>
-                    <textarea className={stylesCorpo.insertText2} name="recomendacoes" placeholder="Recomendações" cols="50" row="10" onChange = {handleEntry}></textarea>
+                    <textarea className={stylesCorpo.insertText2} name="recomendacoes" placeholder="Recomendações." cols="50" row="10" onChange = {handleEntry}></textarea>
                     <p></p>
                     <button className={stylesCorpo.input} type="submit">Atualizar</button>
                 </form>
